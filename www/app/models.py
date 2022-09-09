f89dc4999dce7ea3d63d43e50002f098ae0097bd
Rida Dzhaafar <38199959+rdzhaafar@users.noreply.github.com>
2022-09-09 13:09:47 +0300

    overrode FileField.url property

--- www/app/models.py
+++ www/app/models.py
@@ -1,6 +1,13 @@
 from django.db import models
+from django.urls import reverse
 
 from os import path
+
+
+class FileField(models.FileField):
+    @property
+    def url(self):
+        return reverse("media", kwargs={"pk": self.instance.pk})
 
 
 class SomeFile(models.Model):
